5b0172b87341e4f01a5ce1e00bccc7d38a097c57
Robert Michalski <robertmichalski1995@gmail.com>
2021-03-23 19:05:18 +0100

    loading environment variables

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://project-react-695c0-default-rtdb.firebaseio.com/'
+  baseURL: process.env.REACT_APP_DATABASE
 });
 
 export default instance;
